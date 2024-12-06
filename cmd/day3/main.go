7cc5b65c85e200b181325459b7de2a84a73dbbe3
Ben Marshall <benmarshall.dev@gmail.com>
2024-12-05 20:17:30 -0500

    day 3 part 1/2 completed

--- cmd/day3/main.go
+++ cmd/day3/main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,7 +15,12 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	parseInput(input)
+	inputSlice, err := parseInput(input)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sum := findSum(inputSlice)
+	fmt.Println(sum)
 }
 
 func readInput() (string, error) {
@@ -24,7 +32,7 @@ func readInput() (string, error) {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		input = scanner.Text()
+		input += scanner.Text()
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -34,16 +42,48 @@ func readInput() (string, error) {
 	return input, nil
 }
 
-func parseInput(input string) []string {
-	fmt.Println(input)
-	for i, v := range input {
-		fmt.Println(i, v)
+func parseInput(input string) ([][]uint, error) {
+	re, err := regexp.Compile("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)")
+	if err != nil {
+		return nil, err
 	}
-	// input = strings.FieldsFunc(line, func(r rune) bool {
-	// 	if r == '(' || r == ')' {
-	// 		return true
-	// 	}
-	// 	return false
-	// })
-	return nil
+	parsed := re.FindAllString(input, -1)
+
+	var inputSlice [][]uint
+	var enabled bool = true
+	for _, v := range parsed {
+		var uintSlice []uint
+		var cutString string
+		if v == "do()" {
+			enabled = true
+		}
+		if v == "don't()" {
+			enabled = false
+		}
+		if v != "do()" && v != "don't()" {
+			cutString, _ = strings.CutPrefix(v, "mul(")
+			cutString, _ = strings.CutSuffix(cutString, ")")
+			stringSlice := strings.Split(cutString, ",")
+			for _, v := range stringSlice {
+				converted, err := strconv.Atoi(v)
+				if err != nil {
+					return nil, err
+				}
+				uintSlice = append(uintSlice, uint(converted))
+			}
+			if enabled == true {
+				inputSlice = append(inputSlice, uintSlice)
+			}
+		}
+	}
+	return inputSlice, nil
+}
+
+func findSum(input [][]uint) uint {
+	var sum uint
+	for _, v := range input {
+		product := v[0] * v[1]
+		sum += product
+	}
+	return sum
 }
